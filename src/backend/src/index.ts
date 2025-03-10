5d78324160a9610c421993989a331eccea3832ae
Mike <michael.czak@edu.fh-joanneum.at>
2025-03-10 11:01:02 +0100

    use dotenv

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -1,12 +1,8 @@
+import 'dotenv/config';
 import express from 'express';
-import mongoose from 'mongoose';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-mongoose.connect('mongodb://admin:example@db:27017')
-  .then(() => console.log("Connected to the database"))
-  .catch(err => console.error("Database connection error", err));
 
 app.get('/', (req, res) => {
   res.send("Hello from the backend!");
