5bc101f519713c9634dd1aec64b71db88c430269
Czak Michael <michael.czak@edu.fh-joanneum.at>
2025-03-30 20:37:11 +0200

    WIP add route for saving a form

--- src/backend/src/controller/FormController.ts
+++ src/backend/src/controller/FormController.ts
@@ -2,6 +2,9 @@ import { Request, Response } from 'express';
 import IFormloader from '../util/IFormLoader';
 import RawFormLoader from '../util/RawFormLoader';
 import { NextFunction } from 'express-serve-static-core';
+import * as fs from 'fs';
+import * as path from 'path';
+import 'crypto';
 
 interface FormParams {
   name: string;
@@ -23,5 +26,15 @@ export default class FormController {
       next(error);
     }
 
+  }
+
+  public static async newForm(form: string): Promise<void> {
+    // const timestamp = new Date().toISOString()
+    const uuid = crypto.randomUUID()
+    const filePath = path.join(__dirname, '../../data', `${uuid}.json`);
+    fs.writeFile(filePath, form, 'utf-8', (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    });
   }
 }
