76c09f9b2f6e0f1aa279681ba7b4930e573367a7
Czak Michael <michael.czak@edu.fh-joanneum.at>
2025-04-18 00:51:08 +0200

    add dialog for personal info

--- src/frontend/src/smartforms-ui-frontend.ts
+++ src/frontend/src/smartforms-ui-frontend.ts
@@ -5,8 +5,10 @@ import './components/form-greeting.js';
 import './components/theme-toggle.js';
 import './components/form-renderer/smart-form.js';
 import './components/form-renderer/material-form.js';
-import ApiClient from './util/ApiClient.js';
+import { MdDialog } from '@material/web/all.js';
+import {EASING} from '@material/web/internal/motion/animation.js';
 import { FormType } from './FormType.js';
+import ApiClient from './util/ApiClient.js';
 
 @customElement('smartforms-ui-frontend')
 export class SmartformsUiFrontend extends LitElement {
@@ -28,23 +30,58 @@ export class SmartformsUiFrontend extends LitElement {
       margin: 32px auto;
       flex-grow: 1;
     }
+    #personal-info-button {
+      position: fixed;
+      bottom: 56px;
+      right: 16px;
+    }
+    #personal-info-button.highlighted::before {
+      content: 'south_east';
+      margin-top: -48px;
+      font-family:"Material Icons";
+      font-size: 50px;
+      font-weight: bold;
+      color: var(--required);
+      animation: jumping .5s infinite ease-in-out alternate
+    }
+
+    @keyframes jumping {
+      from {transform: translateX(-20px) translateY(-20px)}
+      to {transform: translateX(0) translateY(0)}
+    }
+    #personal-info {
+      margin: auto 16px 132px auto;
+    }
+
   `;
 
   render() {
-      return !this._greetingRead ? html`
+      return (this._rawForm && this._formType && html`
+      ${!this._greetingRead ? html`
         <main>
           <form-greeting @submit=${this.handleGreetingSubmit}></form-greeting>
         </main>
       ` :
-     (this._rawForm && this._formType && html`
-        ${choose(this._formType, [
+        choose(this._formType, [
           [FormType.MATERIAL, () => html`<material-form .formData=${this._rawForm}></material-form>`],
           [FormType.SMART, () => html`<smart-form .formData=${this._rawForm}></smart-form>`],
         ]
         )}
-      `) || html`
-      <pre>loading form data...</pre>`
-      ;
+        <md-fab @click=${this.showPersonalData} id="personal-info-button" class="highlighted" aria-label="Your personal data">
+          <my-icon slot="icon" icon="info"></my-icon>
+        </md-fab>
+        <md-dialog id="personal-info">
+          <span slot="headline">
+            <span style="flex: 1;">Your personal data</span>
+            <md-icon-button form="form" value="close" aria-label="Close dialog">
+              <my-icon icon="close"></my-icon>
+            </md-icon-button>
+          </span>
+          <form id="form" slot="content" method="dialog">
+            Your Birthday: 20.10.2000
+          </form>
+        </md-dialog>
+      `) || html`<pre>loading form data...</pre>`;
   }
 
   connectedCallback(): void {
@@ -52,6 +89,23 @@ export class SmartformsUiFrontend extends LitElement {
     super.connectedCallback();
     this.loadFormData();
   }
+
+  private showPersonalData(event: Event): void {
+    const target = event.target as Element;
+    target.classList.remove("highlighted");
+    const dialog = target.getRootNode() instanceof ShadowRoot
+        ? (target.getRootNode() as ShadowRoot).querySelector('md-dialog')
+        : target.nextElementSibling as MdDialog;
+    if(dialog) {
+      const open = dialog.getOpenAnimation();
+      open.dialog = [[[{'transform': 'translateX(150px)'}, {'transform': 'translateX(0)'}], {duration: 500, easing: EASING.EMPHASIZED}]]
+      const close = dialog.getCloseAnimation();
+      console.log(JSON.stringify(close));
+      close.dialog = [[[{"transform":"translateX(0)"},{"transform":"translateX(150px)"}],{"duration":150,"easing":"cubic-bezier(.3,0,.8,.15)"}]]
+    }
+
+    dialog?.show();
+}
 
   private async loadFormData() {
     this._rawForm = await ApiClient.loadFormData('registration');
