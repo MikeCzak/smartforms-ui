7505e3d22ec9a6c4a3297b62d38961a58752a3cd
Czak Michael <michael.czak@edu.fh-joanneum.at>
2025-04-28 08:38:25 +0200

    WIP: feedback form in thank-you component

--- src/frontend/src/components/thank-you.ts
+++ src/frontend/src/components/thank-you.ts
@@ -1,4 +1,5 @@
 import { LitElement, html, css } from 'lit';
+import { when } from 'lit/directives/when.js';
 import { customElement, property, state } from 'lit/decorators.js';
 import './my-icon.js';
 import ApiClient from '../util/ApiClient.js';
@@ -10,35 +11,48 @@ export default class ThankYou extends LitElement {
   @property({type: String, attribute: false}) internalFormId!: string;
 
   static styles = css`
-    .wrapper {
+    .thank-you, .feedback {
       display: flex;
       flex-direction: column;
       justify-content: center;
       align-items: center;
       font-size: 20px;
+      width: 100%;
       text-align: center;
+      margin-bottom: 100px;
     }
     my-icon.success {
       color: green;
       font-size: 40px;
     }
     my-icon.feedback-icon {
-      font-size: 120px;
+      font-size: 80px;
     }
   `;
 
   render() {
     return html`
-    <div class="wrapper">
+    <div class="thank-you">
       <p><my-icon icon="task_alt" class="success"></my-icon> Form successfully submitted.</p>
       <p>
         Thank you SO MUCH for your time!<br>
-        You're almost there - please provide some valuable feedback about the form
-        filling experience in the feedback form below!
+        You're almost there - please provide some feedback about the form
+        filling experience in the form below!
       </p>
-      <my-icon icon="chat" class="feedback-icon"></my-icon>
     </div>
-    <smart-form .formData=${this._feedbackForm}></smart-form>
+    ${when(
+      !this._feedbackForm,
+      () => html`
+        <div class="feedback">
+          <h1>
+            <my-icon icon="chat" class="feedback-icon"></my-icon>
+            Feedback
+          </h1>
+          <smart-form .formData=${this._feedbackForm}></smart-form>
+        </div>
+      `,
+      () => html`<md-circular-progress indeterminate></md-circular-progress> loading feedback form...`
+    )}
     `;
   }
 
