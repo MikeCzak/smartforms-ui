0e31ed6960ffc547fa5a9ada0b12f1ed0c702488
Czak Michael <michael.czak@edu.fh-joanneum.at>
2025-05-01 18:07:11 +0200

    remove unused code

--- src/frontend/src/components/form-renderer/IForm.ts
+++ src/frontend/src/components/form-renderer/IForm.ts
@@ -1,10 +1,6 @@
 import { HTMLTemplateResult } from "lit";
-import IFormElement from "../form-element/IFormElement.js";
 
 export default interface IForm {
-  // addFormElement(element: IFormElement): IForm;
-  // getFormElements(): Array<IFormElement>;
-  // getFormElementById(id: string): IFormElement|null;
 
   render(): HTMLTemplateResult;
 }
